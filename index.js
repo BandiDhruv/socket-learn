1a1e114d6eb0a02235d0ec11e637f9637e0d327a
Dhruv Bandi <dhruvbandi03@gmail.com>
2024-11-15 15:55:47 +0530

    send message done

--- index.js
+++ index.js
@@ -13,6 +13,19 @@ app.get('/',function(req,res){
 })
 io.on('connection',function(socket){
     console.log('a user connected');
+
+    // setTimeout(function(){
+    //     socket.send("Sent message from server by prereserved events");
+    // },1500)
+    // setTimeout(function(){
+    //     socket.emit("myCustomEvent",{
+    //         data:"Hello this is dummy data"            
+    //     });
+    // },1500)
+    
+    socket.on("myCustomEventFromClientSide",function(data){
+        console.log(data);
+    })
     socket.on('disconnect',function(){
         console.log('a user disconnected');
     })
