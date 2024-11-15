5ef57420bfb5147f1b659f440f33272884072c52
Dhruv Bandi <dhruvbandi03@gmail.com>
2024-11-15 16:14:34 +0530

    broadcasting via sockets

--- index.js
+++ index.js
@@ -11,22 +11,31 @@ app.get('/',function(req,res){
     var filename = "index.html"
     res.sendFile(filename,options);
 })
-io.on('connection',function(socket){
-    console.log('a user connected');
 
+var users=0;
+
+io.on('connection',function(socket){
+    users++;
+    console.log('a user connected');
+    socket.emit('newUserConnected',{message:"Hello"});
+    socket.broadcast.emit('newUserConnected',{message:users + "users connected"});
     // setTimeout(function(){
-    //     socket.send("Sent message from server by prereserved events");
-    // },1500)
-    // setTimeout(function(){
-    //     socket.emit("myCustomEvent",{
-    //         data:"Hello this is dummy data"            
-    //     });
-    // },1500)
+        //     socket.send("Sent message from server by prereserved events");
+        // },1500)
+        // setTimeout(function(){
+            //     socket.emit("myCustomEvent",{
+                //         data:"Hello this is dummy data"            
+                //     });
+                // },1500)
     
-    socket.on("myCustomEventFromClientSide",function(data){
-        console.log(data);
-    })
-    socket.on('disconnect',function(){
+    // socket.on("myCustomEventFromClientSide",function(data){
+        //     console.log(data);
+        // })
+        io.sockets.emit('broadcast',{message :  users + "users connected!"})
+        socket.on('disconnect',function(){
+        users--;
+        socket.broadcast.emit('newUserConnected',{message:users  + "users connected"});
+        // io.sockets.emit('broadcast',{message :  users + "users connected!"})
         console.log('a user disconnected');
     })
 })
