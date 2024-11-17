139761c06c549ea64276f09164b18020fd489f00
Dhruv Bandi <dhruvbandi03@gmail.com>
2024-11-17 11:57:08 +0530

    namespace working understood

--- index.js
+++ index.js
@@ -14,29 +14,39 @@ app.get('/',function(req,res){
 
 var users=0;
 
-io.on('connection',function(socket){
-    users++;
-    console.log('a user connected');
-    socket.emit('newUserConnected',{message:"Hello"});
-    socket.broadcast.emit('newUserConnected',{message:users + "users connected"});
-    // setTimeout(function(){
-        //     socket.send("Sent message from server by prereserved events");
-        // },1500)
-        // setTimeout(function(){
-            //     socket.emit("myCustomEvent",{
-                //         data:"Hello this is dummy data"            
-                //     });
-                // },1500)
+// io.on('connection',function(socket){
+//     users++;
+//     console.log('a user connected');
+//     socket.emit('newUserConnected',{message:"Hello"});
+//     socket.broadcast.emit('newUserConnected',{message:users + "users connected"});
+//     // setTimeout(function(){
+//         //     socket.send("Sent message from server by prereserved events");
+//         // },1500)
+//         // setTimeout(function(){
+//             //     socket.emit("myCustomEvent",{
+//                 //         data:"Hello this is dummy data"            
+//                 //     });
+//                 // },1500)
     
-    // socket.on("myCustomEventFromClientSide",function(data){
-        //     console.log(data);
-        // })
-        io.sockets.emit('broadcast',{message :  users + "users connected!"})
-        socket.on('disconnect',function(){
-        users--;
-        socket.broadcast.emit('newUserConnected',{message:users  + "users connected"});
-        // io.sockets.emit('broadcast',{message :  users + "users connected!"})
-        console.log('a user disconnected');
+//     // socket.on("myCustomEventFromClientSide",function(data){
+//         //     console.log(data);
+//         // })
+//         io.sockets.emit('broadcast',{message :  users + "users connected!"})
+//         socket.on('disconnect',function(){
+//         users--;
+//         socket.broadcast.emit('newUserConnected',{message:users  + "users connected"});
+//         // io.sockets.emit('broadcast',{message :  users + "users connected!"})
+//         console.log('a user disconnected');
+//     })
+// })
+
+var cnsp = io.of('/custom-namespace')
+
+cnsp.on('connection',function(socket){
+    console.log("A user Connected");
+    cnsp.emit('testEvent',"Tester Event Called");
+    socket.on('disconnect',function(){
+        console.log("A user Disconnected");
     })
 })
 http.listen(5000,function(){
